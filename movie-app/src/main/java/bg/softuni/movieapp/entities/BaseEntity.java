ef9cbd5baf132354d42e698f31b465d2b8f0d793
D3Y4N <103333790+Deyan2306@users.noreply.github.com>
2023-11-21 08:46:59 +0200

    initial commit

---
+++ movie-app/src/main/java/bg/softuni/movieapp/entities/BaseEntity.java
@@ -0,0 +1,21 @@
+package bg.softuni.movieapp.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter @Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name="UUID", strategy = "uuid2")
+    private UUID id;
+
+}
---
+++ movie-app/src/test/java/bg/softuni/movieapp/MovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.movieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
