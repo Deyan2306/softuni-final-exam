3b076bfab2b8badc99ca330bb448611ec72dd48d
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-11-25 19:35:25 +0200

    some new classes

--- movie-app/src/main/java/bg/softuni/movieapp/model/entity/Actor.java
+++ movie-app/src/main/java/bg/softuni/movieapp/model/entity/Actor.java
@@ -1,4 +1,30 @@
 package bg.softuni.movieapp.model.entity;
 
-public class Actor {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authors")
+public class Actor extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthdate;
+    private LocalDate deathDate;
+    private String authorPhotoURI;
+    private String personalYoutubeVideoID;
+    private String biography;
+    private Integer height;
+    private List<String> alternativeNames;
+    private List<ActorRole> roles;
+    private List<Comment> comments;
+
 }
---
+++ movie-app/src/main/java/bg/softuni/movieapp/model/entity/Comment.java
@@ -0,0 +1,4 @@
+package bg.softuni.movieapp.model.entity;
+
+public class Comment {
+}
