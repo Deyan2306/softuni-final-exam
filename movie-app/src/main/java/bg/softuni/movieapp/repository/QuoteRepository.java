fb8a99c078486933adbc10b34500dff57ad63d59
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-11-26 20:45:51 +0200

    quote repository is done

---
+++ movie-app/src/main/java/bg/softuni/movieapp/repository/QuoteRepository.java
@@ -0,0 +1,11 @@
+package bg.softuni.movieapp.repository;
+
+import bg.softuni.movieapp.model.entity.Quote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface QuoteRepository extends JpaRepository<Quote, UUID> {
+}
