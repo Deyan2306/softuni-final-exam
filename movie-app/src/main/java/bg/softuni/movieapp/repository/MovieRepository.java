c29f4b98c289619336b92d79f9339e3751e105d2
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-11-26 20:45:12 +0200

    movie repository is done

---
+++ movie-app/src/main/java/bg/softuni/movieapp/repository/MovieRepository.java
@@ -0,0 +1,11 @@
+package bg.softuni.movieapp.repository;
+
+import bg.softuni.movieapp.model.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, UUID> {
+}
