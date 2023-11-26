929a4de1db769c573716ee6325b90ddec4bcef08
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-11-26 20:43:53 +0200

    comment repository is done

--- movie-app/src/main/java/bg/softuni/movieapp/repository/CommentRepository.java
+++ movie-app/src/main/java/bg/softuni/movieapp/repository/CommentRepository.java
@@ -1,11 +1,11 @@
 package bg.softuni.movieapp.repository;
 
-import bg.softuni.movieapp.model.entity.Role;
+import bg.softuni.movieapp.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, UUID> {
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
 }
