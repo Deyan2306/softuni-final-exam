43d1351e89341970cc79350198dc8e629f209d52
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-11-27 21:07:03 +0200

    name refactoring

--- movie-app/src/main/java/bg/softuni/movieapp/services/impl/TVSeriesServiceImpl.java
+++ movie-app/src/main/java/bg/softuni/movieapp/services/impl/TVSeriesServiceImpl.java
@@ -5,12 +5,12 @@ import bg.softuni.movieapp.services.TVSeriesEpisodeService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TVSeriesEpisodeImpl implements TVSeriesEpisodeService {
+public class TVSeriesServiceImpl implements TVSeriesEpisodeService {
 
     private final TVSeriesEpisodeRepository tvSeriesEpisodeRepository;
     private final LoggedUser loggedUser;
 
-    public TVSeriesEpisodeImpl(TVSeriesEpisodeRepository tvSeriesEpisodeRepository, LoggedUser loggedUser) {
+    public TVSeriesServiceImpl(TVSeriesEpisodeRepository tvSeriesEpisodeRepository, LoggedUser loggedUser) {
         this.tvSeriesEpisodeRepository = tvSeriesEpisodeRepository;
         this.loggedUser = loggedUser;
     }
