ef5130cc2cdfdc507e4913415f77a0754e46ac20
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-11-29 00:39:56 +0200

    thymeleaf authentication

--- movie-app/src/main/java/bg/softuni/movieapp/security/WebSecurityConfig.java
+++ movie-app/src/main/java/bg/softuni/movieapp/security/WebSecurityConfig.java
@@ -5,21 +5,13 @@ import bg.softuni.movieapp.services.impl.MovieAppUserDetailsService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,8 +22,8 @@ public class WebSecurityConfig {
         http
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/", "/home").permitAll()
-                        .requestMatchers("/users/register", "/users/login").permitAll()
-                        .requestMatchers("/users/process-login", "/users/login-error").permitAll()
+                        .requestMatchers("/users/register", "/users/login", "/users/logout").permitAll()
+                        .requestMatchers("/users/logout-success", "/users/login-error").permitAll()
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .anyRequest().authenticated()
                 )
@@ -47,7 +39,7 @@ public class WebSecurityConfig {
                         .logoutSuccessUrl("/users/logout-success")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
-                        .permitAll())
+                )
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
